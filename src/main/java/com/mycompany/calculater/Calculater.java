f76c4980f8fa97aa6473775176438f6182188a56
Zohaib khan <81791963+zohaibkhan04@users.noreply.github.com>
2023-05-25 23:34:13 +0500

    Calculator

---
+++ src/main/java/com/mycompany/calculater/Calculater.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculater;
+
+/**
+ *
+ * @author Zohaib khan
+ */
+public class Calculater {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
